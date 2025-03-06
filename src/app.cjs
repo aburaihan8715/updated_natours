6f950bb027f7797090689f3296b6984c0863f2f5
aburahan8721 <aburaihan8721@gmail.com>
2025-03-06 19:27:50 +0600

    done

--- src/app.cjs
+++ src/app.cjs
@@ -1,96 +1,5 @@
 /* eslint-disable no-undef */
 
-/*
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const hpp = require('hpp');
-const cookieParser = require('cookie-parser');
-const compression = require('compression');
-const cors = require('cors');
-
-const globalErrorHandler = require('./controllers/errorController.cjs');
-const tourRouter = require('./routes/tourRoutes.cjs');
-const userRouter = require('./routes/userRoutes.cjs');
-const reviewRouter = require('./routes/reviewRoutes.cjs');
-const bookingRouter = require('./routes/bookingRoutes.cjs');
-const viewRouter = require('./routes/viewRoutes.cjs');
-const AppError = require('./utils/appError.cjs');
-
-// Start express app
-const app = express();
-
-// app.enable('trust proxy');
-
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
-
-// 1) GLOBAL MIDDLEWARES
-
-app.use(cors());
-
-// Serving static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Set security HTTP headers
-app.use(helmet({ contentSecurityPolicy: false }));
-
-// Development logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
-// Limit requests from same API
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!',
-});
-app.use('/api', limiter);
-
-// Body parser, reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
-app.use(cookieParser());
-
-// Data sanitization against NoSQL query injection
-app.use(mongoSanitize());
-
-// Prevent parameter pollution
-app.use(
-  hpp({
-    whitelist: [
-      'duration',
-      'ratingsQuantity',
-      'ratingsAverage',
-      'maxGroupSize',
-      'difficulty',
-      'price',
-    ],
-  }),
-);
-
-app.use(compression());
-
-// 3) ROUTES
-app.use('/', viewRouter);
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/reviews', reviewRouter);
-app.use('/api/v1/bookings', bookingRouter);
-
-app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-});
-
-app.use(globalErrorHandler);
-
-module.exports = app;
-
-*/
 const express = require('express');
 const { status: httpStatus } = require('http-status');
 const morgan = require('morgan');
@@ -109,8 +18,15 @@ const globalErrorHandler = require('./middlewares/globalErrorHandler.cjs');
 const AppError = require('./errors/appError.cjs');
 const viewRouter = require('./routes/viewRoutes.cjs');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
 
 const app = express();
+
+// app.use(compression());
+app.use(compression({ threshold: 0 }));
+
+// cors
+app.use(cors());
 
 // set view engine and view path
 app.set('view engine', 'pug');
@@ -189,7 +105,6 @@ app.get('/test', (req, res) => {
 
 // view route
 app.use('/', viewRouter);
-
 // api routes
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
